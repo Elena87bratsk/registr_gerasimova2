15f4e0f2251dc7613dbe1133235d5a21f7bf63bb
elena <elena87bratsk@mail.ru>
2021-06-05 09:33:21 +0300

    ghbhdfjsh

--- registr_gerasimova/ContentView.swift
+++ registr_gerasimova/ContentView.swift
@@ -6,12 +6,63 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State var mail = ""
+    @State var pas = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack( content: {
+            
+            Image("Pl")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 150, height: 150, alignment:.center)
+                .padding()
+            Text("EMAIL")
+                .frame(width: 300, height: 0, alignment: .leading)
+            HStack{
+                Image (systemName: "envelope")
+                TextField("name@email.com",text:$mail)
+                   .padding(.horizontal,20)
+                 
+                    
+            }
+            .overlay(RoundedRectangle (cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).stroke(Color (UIColor.blue).opacity(1),lineWidth: 1))
+            .padding(.leading, 20)
+            Text("PASSWORD")
+                .font(.system(size: 12))
+                .frame(width: 300, height: 0, alignment: .leading)
+            
+            
+            HStack {
+                Image (systemName: "lock")
+                TextField("********", text:$pas)
+                .padding(.horizontal,20)
+            }
+            .overlay(RoundedRectangle (cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).stroke(Color (UIColor.blue).opacity(1),lineWidth: 1))
+            .padding(.leading, 20)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("SIGN UP")
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 40, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .background(Color(.blue))
+                    .cornerRadius(10)
+                
+            })
+            HStack{
+                Text("Already have an account?")
+                    .foregroundColor(.gray)
+                    .frame(width: 200, height: 20, alignment: .leading)
+                
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        Text("Login")
+                            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                })
+            }
+        })
     }
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +70,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
